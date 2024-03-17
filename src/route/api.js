4339310daa544835943a6224a74517f47580ad20
ahadalichowdhury <smahadalichowdhury@gmail.com>
2024-03-17 14:42:46 +0600

    added: upload image functionality

--- src/route/api.js
+++ src/route/api.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
+const multer = require("multer");
+const uploadImageUtils = require("../utils/uploadImageUtils");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 
 router.post("/signUp", userController.userRegistration);
 router.post("/varify-email", userController.verifyRegistrationOTP);
 
 router.post("/analyze-document", userController.analyzeDocument);
+
+router.post("/upload", upload.single("file"), uploadImageUtils.uploadImage);
 
 module.exports = router;
