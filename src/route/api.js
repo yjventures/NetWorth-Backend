91ce81c05d09c8afedcf0bb350b5387ce86823d8
Naimur <naeemhasan28@gmail.com>
2024-03-18 11:18:54 +0600

    fixed route name typo

--- src/route/api.js
+++ src/route/api.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controller/userController");
-const multer = require("multer");
-const uploadImageUtils = require("../utils/uploadImageUtils");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const authVerifyMiddleware = require("../middleware/AuthVerifyMiddleware")
+const express = require('express')
+const router = express.Router()
+const userController = require('../controller/userController')
+const multer = require('multer')
+const uploadImageUtils = require('../utils/uploadImageUtils')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+const authVerifyMiddleware = require('../middleware/AuthVerifyMiddleware')
 
-router.post("/signUp", userController.userRegistration);
-router.post("/varify-email", userController.verifyRegistrationOTP);
+router.post('/signUp', userController.userRegistration)
+router.post('/verify-email', userController.verifyRegistrationOTP)
 
-router.post("/analyze-document", userController.analyzeDocument);
+router.post('/analyze-document', userController.analyzeDocument)
 
-router.post("/upload", upload.single("file"), uploadImageUtils.uploadImage);
+router.post('/upload', upload.single('file'), uploadImageUtils.uploadImage)
 
-router.post("/user/login", userController.login);
-router.post("/user/varify-login", userController.verifyLoginOTP);
-router.post("/user/access-token", userController.generateAccessToken)
-router.put("/user/personal-info", authVerifyMiddleware.authMiddleware, userController.updatePersonalInfo)
-module.exports = router;
+router.post('/user/login', userController.login)
+router.post('/user/verify-login', userController.verifyLoginOTP)
+router.post('/user/access-token', userController.generateAccessToken)
+router.put('/user/personal-info', authVerifyMiddleware.authMiddleware, userController.updatePersonalInfo)
+module.exports = router
