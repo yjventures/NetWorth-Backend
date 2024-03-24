4624ceadff3b810ca4ac9ead1bf6d1a7da6048ba
Naimur <naeemhasan28@gmail.com>
2024-03-24 12:58:02 +0600

    updated personal info model

--- src/model/personalInfoModel.js
+++ src/model/personalInfoModel.js
@@ -6,13 +6,9 @@ const personalInfoSchema = mongoose.Schema(
     address: String,
     date_of_birth: String,
     profile_image: String,
-    // cover_image: String,
     used_token: Number,
     phone_number: String,
     bio: String,
-    // company_name: String,
-    // company_logo: Number,
-    designation: String,
     number_of_connections: Number,
   },
   {
