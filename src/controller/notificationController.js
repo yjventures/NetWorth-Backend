91476302b619e18f55b70f4db2b07f6818e525cb
ahadalichowdhury <smahadalichowdhury@email.com>
2024-04-02 16:10:14 +0600

    showAllNotifications controller created

---
+++ src/controller/notificationController.js
@@ -0,0 +1,21 @@
+const { catchAsync } = require("../middleware/catchAsyncError");
+const cardModel = require("../model/cardModel");
+
+exports.showAllNotifications = catchAsync(async (req, res, next) => {
+  const cardId = req.params.id
+
+  try {
+    const card = await cardModel.findById(cardId).populate({ path: 'notifications', model: 'Notification' })
+
+    if (!card) {
+      return next(new ErrorHandler(404, "Something Wrong with Card"));
+    }
+
+    return res.status(200).json({
+      status: true,
+      data: card.notifications,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(500, "Something Wrong with Card"));
+  }
+})
