0ae1932583b16d86245ad8e1f15cb254ce29f45d
ahadalichowdhury <smahadalichowdhury@email.com>
2024-04-21 16:41:37 +0600

    modified: searchContact controller

--- src/controller/connectionController.js
+++ src/controller/connectionController.js
@@ -12,58 +12,67 @@ const notificationModel = require("../model/notificationModel");
 const { sendMultiplePushNotification } = require("../utils/fcmUtils");
 
 exports.searchContact = catchAsync(async (req, res, next) => {
-  const userId = req.headers.userId;
+  try {
+    const userId = req.headers.userId;
 
-  const { search, country, city, designation } = req.query;
-  let query = {};
+    const { search, country, city, designation } = req.query;
+    let query = {};
 
-  if (search) {
-    if (search.includes("@")) {
-      query.email = { $regex: `^${search}`, $options: "i" };
-    } else {
-      query.$or = [
-        { name: { $regex: search, $options: "i" } },
-        { email: { $regex: search, $options: "i" } },
-      ];
+    if (search) {
+      if (search.includes("@")) {
+        query.email = { $regex: `^${search}`, $options: "i" };
+      } else {
+        query.$or = [
+          { name: { $regex: search, $options: "i" } },
+          { email: { $regex: search, $options: "i" } },
+        ];
+      }
     }
+
+    if (designation) {
+      query.designation = { $regex: designation, $options: "i" };
+    }
+
+    // For city and country
+    if (city && country) {
+      const cityRegex = new RegExp(city, "i");
+      const countryRegex = new RegExp(country, "i");
+      query.address = {
+        $regex: `${cityRegex.source}.*${countryRegex.source}`,
+        $options: "i",
+      };
+    } else if (city) {
+      const cityRegex = new RegExp(city, "i");
+      query.address = {
+        $regex: cityRegex.source,
+        $options: "i",
+      };
+    } else if (country) {
+      const countryRegex = new RegExp(country, "i");
+      query.address = {
+        $regex: countryRegex.source,
+        $options: "i",
+      };
+    }
+
+    // Exclude user's own cards
+    const user = await userModel.findById(userId);
+    const userCardIds = user.cards.map((card) => card.toString());
+    query._id = { $nin: userCardIds };
+
+    // Execute the query
+    const searchResult = await cardModel.find(query);
+    // const total = searchResult.length;
+
+    res.status(200).json({
+      status: true,
+      data: searchResult,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  if (designation) {
-    query.designation = { $regex: designation, $options: "i" };
-  }
-
-  // For city and country
-  if (city && country) {
-    const cityRegex = new RegExp(city, "i");
-    const countryRegex = new RegExp(country, "i");
-    query.address = {
-      $regex: `${cityRegex.source}.*${countryRegex.source}`,
-      $options: "i",
-    };
-  } else if (city) {
-    const cityRegex = new RegExp(city, "i");
-    query.address = {
-      $regex: cityRegex.source,
-      $options: "i",
-    };
-  } else if (country) {
-    const countryRegex = new RegExp(country, "i");
-    query.address = {
-      $regex: countryRegex.source,
-      $options: "i",
-    };
-  }
-
-  // console.log(query);
-
-  // Execute the query
-  const searchResult = await cardModel.find(query);
-
-  res.status(200).json({
-    status: true,
-    data: searchResult,
-  });
 });
+
 
 //send friend request
 exports.sendConnectionRequest = catchAsync(async (req, res, next) => {
